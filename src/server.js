e0785d1ac51648131fbe58a3d00870c0007767f9
Igor Silva <osilvaigor@gmail.com>
2022-09-03 17:40:19 -0300

    feat: add openAPI document

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const documentation = require('./openapi.json')
+
 const PORT = process.env.PORT || 4242
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(documentation))
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
