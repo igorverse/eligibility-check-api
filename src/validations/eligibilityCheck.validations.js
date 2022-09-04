b6da30cf61295716d7d71d5f2853faf66fee85ee
Igor Silva <osilvaigor@gmail.com>
2022-09-03 23:21:50 -0300

    feat: add eligibility check validations

--- src/validations/eligibilityCheck.validations.js
+++ src/validations/eligibilityCheck.validations.js
@@ -1,13 +1,27 @@
-const isElegibleConsumptionClass = () => {
-  throw 'Not implemented yet'
+const isElegibleConsumptionClass = (consumptionClass) => {
+  const elegiblesConsumptionClasses = ['comercial', 'residencial', 'industrial']
+
+  return elegiblesConsumptionClasses.includes(consumptionClass)
 }
 
-const isElegibleTariffModality = () => {
-  throw 'Not implemented yet'
+const isElegibleTariffModality = (tariffModality) => {
+  const elegibleTarfiffModalities = ['convencional', 'branca']
+
+  return elegibleTarfiffModalities.includes(tariffModality)
 }
 
-const isElegibleMinimumConsumption = () => {
-  throw 'Not implemented yet'
+const isElegibleMinimumConsumption = (connectionType, consumptionHistory) => {
+  const averageConsumption =
+    consumptionHistory.reduce((acc, curr) => acc + curr) /
+    consumptionHistory.length
+
+  const minimiumConsumtionMatch = {
+    monofasica: averageConsumption > 400,
+    bifasica: averageConsumption > 500,
+    trifasica: averageConsumption > 750,
+  }
+
+  return minimiumConsumtionMatch[connectionType]
 }
 
 module.exports = {
